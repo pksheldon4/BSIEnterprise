ad610b4912aa353783a17d064051292ccf30e077
franktylerva <franktylerva@gmail.com>
2023-10-30 14:14:27 -0400

    Configure OIDC for Client application.

--- BSIClient/Program.cs
+++ BSIClient/Program.cs
@@ -1,11 +1,25 @@
+using BSIEnterprise.BSIClient;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BSIEnterprise.BSIClient;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.MetadataUrl = "http://localhost:8080/realms/bsi/.well-known/openid-configuration";
+    options.ProviderOptions.Authority = "http://localhost:8080/realms/bsi";
+    options.ProviderOptions.ClientId = "bsi-client";
+    options.ProviderOptions.ResponseType = "id_token token";
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+
+    options.UserOptions.NameClaim = "preferred_username";
+    options.UserOptions.RoleClaim = "roles";
+    options.UserOptions.ScopeClaim = "scope";
+});
 
 await builder.Build().RunAsync();
